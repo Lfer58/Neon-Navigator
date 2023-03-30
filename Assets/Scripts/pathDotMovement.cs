17a31659846e46f7aab085712eae4300ac3a416d
Lfer58 <nishitd20@gmail.com>
2023-03-30 16:34:15 -0700

    Removed line & sphere (Reverse w/ mouse works)

--- Assets/Scripts/pathDotMovement.cs
+++ Assets/Scripts/pathDotMovement.cs
@@ -7,7 +7,6 @@ public class pathDotMovement : MonoBehaviour
 {
     
     public PlayerController player;
-    public Rigidbody myRigidBody;
     
     public float playerBase;
     private float playerY;
@@ -19,33 +18,23 @@ public class pathDotMovement : MonoBehaviour
     public float radiusExtends;
     private float distanceFromPlayer;
 
-    public GameObject cube;
+    public GameObject path;
     private GameObject currentPath; //path instance being generated
     private float pathRotation; // How much the path rotates before extending.
     private float pathRotationBase;
     public float scaleForce;
-    private float pathInversion;
     private int verticalInput;
     private int horizontalDirection;
-    private int verticalDirection;
 
     private float positionEnd;
     private float positionEndX;
     private float positionEndY;
     private Boolean isPathCreated = false;
     private Boolean isPathVerticalTravel = false;
-    
-    public lineScript lineObject;
-    private LineRenderer line;
-    private int i = 0;
-    
-
-    public float snapToBaseRange;
 
     // Start is called before the first frame update
     void Start()
     {
-        line = lineObject.GetComponent<LineRenderer>();
 
         playerRotation = player.transform.rotation;
     }
@@ -86,14 +75,8 @@ public class pathDotMovement : MonoBehaviour
 
     }
 
-    public bool checkRadius() {
-        return transform.position.y < playerY + radiusExtends && transform.position.y > playerY + radiusExtends * -1;
-    }
-
     public void pathCreation() {
-        if (!(transform.position.x > playerX + radiusExtends)) {
-            myRigidBody.velocity = new Vector3(10 * Input.GetAxis("Fire1"), myRigidBody.velocity.y,0);
-        }
+        
         // The second if condition takes advantage of the fact of they order of the functions pathCreation and pathHeight.
         // After letting go of up and down, isPathVerticalTravel isn't negative yet, so this allows us to create a new steady path
         // while not distrubing the whole line, and on calling the pathHeight function, is PathVerticalTravel will become false.
@@ -110,7 +93,7 @@ public class pathDotMovement : MonoBehaviour
             //     pathRotation = 180;
             // }
 
-            currentPath = Instantiate(cube, new Vector3(positionEndX, positionEndY, 0), playerRotation);
+            currentPath = Instantiate(path, new Vector3(positionEndX, positionEndY, 0), playerRotation);
             currentPath.transform.eulerAngles = new Vector3 (0,0, pathRotation);
             isPathCreated = true;
         }
@@ -118,57 +101,32 @@ public class pathDotMovement : MonoBehaviour
         // Once path is created, it'll scale out linearly with the force in scaleForce
         // It should stop after the path reaches a certain distance, but its not yet implemented.
         if (isPathCreated) {
-            line.positionCount = i + 1;
-            line.SetPosition(i++, new Vector3(transform.position.x, transform.position.y - 0.1f, 0));
-            
             // Calculates distance from the base player position so that the path only extends outward to a distance specificied by radiusExtends.
             distanceFromPlayer = (float)Math.Sqrt(Math.Pow(positionEndX - playerX, 2) + Math.Pow(positionEndY - playerY, 2));
             if (distanceFromPlayer < radiusExtends) {
                 currentPath.transform.localScale += new Vector3(Time.deltaTime * scaleForce, 0, 0); //Increases the path in the direction of mouse.
             }
         }
-
-        if (!Input.GetButton("Fire1")) {
-            transform.position = new Vector3(playerX, playerY, 0);
-        }
         pathHeight();
     }
     private void pathHeight() {
-        if (checkRadius()) {
-            myRigidBody.velocity = new Vector3(myRigidBody.velocity.x, Input.GetAxis("Vertical") * 10, 0);
-        }
         // Only works when path is actually being created.
         if (isPathCreated && Input.GetButtonDown("Vertical")) {
             isFaceRight();
             setVerticalInput();
-            pathRotation += 45 * verticalInput;
-            setVerticalInput();
-            currentPath = Instantiate(cube, new Vector3(positionEndX, positionEndY, 0), playerRotation);
+            if (pathRotation < 90 || pathRotation > 270) {
+                pathRotation += 45 * verticalInput;
+            } else {
+                pathRotation += 45 * verticalInput * -1;
+            }
+            currentPath = Instantiate(path, new Vector3(positionEndX, positionEndY, 0), playerRotation);
 
-            // Rotates upwards if positive, or downwards if negative.
-            // Two directional multiplicatives as a form of quadrant balancing.
-            // Ex. if using the up arrows and facing to the right, the values are 1 and -1 respectively.
-                // This then takes the right angle of 180, the added 45, thus making it 225. Then applying
-                // the multiplicatives makes it -225, or 135. Thus, quadrant 2, the desired quadrant. 
-            currentPath.transform.eulerAngles = new Vector3 (0,0, pathRotation * horizontalDirection);
+            // Rotates to the newer path rotation as defined earlier.
+            currentPath.transform.eulerAngles = new Vector3 (0,0, pathRotation);
             isPathVerticalTravel = true;
         }
         if (!Input.GetButton("Vertical")) {
             isPathVerticalTravel = false;
-            returnToBaseY();
-        }
-    }
-
-    public void returnToBaseY() {
-        if (transform.position.y > playerY + snapToBaseRange) 
-        {
-            myRigidBody.velocity = new Vector3(myRigidBody.velocity.x, -17, 0);
-        } else if (transform.position.y < playerY - snapToBaseRange) 
-        {
-            myRigidBody.velocity = new Vector3(myRigidBody.velocity.x, 10, 0);
-        } else 
-        {
-            transform.position = new Vector3(transform.position.x, playerY, 0);
         }
     }
 
@@ -212,7 +170,7 @@ public class pathDotMovement : MonoBehaviour
 
                 positionEndY = (float)(positionEnd * Math.Abs(Math.Sin(pathRotation * Mathf.Deg2Rad))) * verticalInput + currentPath.transform.position.y;
                 isFaceRight();
-                positionEndX = (float)(positionEnd * Math.Abs(Math.Cos(pathRotation * Mathf.Deg2Rad))) * horizontalDirection + currentPath.transform.position.x;
+                positionEndX = (float)(positionEnd * Math.Cos(pathRotation * Mathf.Deg2Rad)) + currentPath.transform.position.x;
             } else {
                 positionEndX = positionEnd * horizontalDirection + currentPath.transform.position.x;
                 positionEndY = currentPath.transform.position.y;
